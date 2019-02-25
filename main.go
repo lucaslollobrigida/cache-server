abde06a4e88c9dd0c4c0a41cd5523df189d08373
Lucas Lollobrigida <lucaslollobrigida@gmail.com>
2019-02-24 22:31:09 -0300

    upgrade to a server

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
+	"github.com/lucaslollobrigida/cache-server/cache"
 	"github.com/valyala/fasthttp"
 )
 
+var (
+	addr *string = flag.String("addr", ":3001", "TCP address")
+)
+
 func main() {
-	fmt.Println("Build Ok")
-	fasthttp.ListenAndServe(":3001", nil)
+	flag.Parse()
+
+	m := make(map[string]*cache.Registry)
+
+	c := cache.Cache{
+		m,
+	}
+
+	router := c.Init()
+
+	fmt.Printf("Service is listen to 0.0.0.0%s\n", *addr)
+	if err := fasthttp.ListenAndServe(*addr, router.Handler); err != nil {
+		fmt.Println(err)
+	}
 }
