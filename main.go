7dcdb6949397f2506a4159795759b7340cfffa1b
Lucas Lollobrigida <lucaslollobrigida@gmail.com>
2019-08-05 23:45:23 -0300

    build: add compose build descriptor
    Add compose file with monitoring environment, including prometheus,
    grafana, node_exporter, etc.
    
    Add sentry logging via client lib and update outdated dependencies.

--- main.go
+++ main.go
@@ -3,17 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 
-	"github.com/lucaslollobrigida/cache-server/caching"
+	"cache-server/caching"
+
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/getsentry/sentry-go"
 	"github.com/valyala/fasthttp"
 )
 
 var (
-	addr = flag.String("addr", ":3001", "TCP address")
+	addr      = flag.String("addr", ":80", "TCP address")
+	dsnSentry = flag.String("dsn", "", "Sentry DSN")
 )
 
 func main() {
 	flag.Parse()
+
+	config := sentry.ClientOptions{
+		Dsn: *dsnSentry,
+	}
+
+	if err := sentry.Init(config); err != nil {
+		panic(err)
+	}
+
+	defer sentry.Flush(time.Second * 5)
 
 	c := caching.Cache{}
 
@@ -21,8 +38,12 @@ func main() {
 
 	c.CleanupExpired()
 
+	go func() {
+		sentry.CaptureException(http.ListenAndServe(":6060", nil))
+	}()
+
 	fmt.Printf("Service is listen to 0.0.0.0%s\n", *addr)
-	if err := fasthttp.ListenAndServe(*addr, router.Handler); err != nil {
-		fmt.Println(err)
+	if err := fasthttp.ListenAndServe(fmt.Sprintf("0.0.0.0%s", *addr), router.Handler); err != nil {
+		sentry.CaptureException(err)
 	}
 }
